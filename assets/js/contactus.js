9b6c30b1efae9c2b5b8cf50e57d33c77b3dbe637
garethcritchley <theymightbegareths@gmail.com>
2026-05-25 16:52:08 +1000

    cleaned up contact to have delay

---
+++ assets/js/contactus.js
@@ -0,0 +1,33 @@
+const scriptURL =
+    "https://script.google.com/macros/s/AKfycbwYl9v8u-O3r7KmWuMktAjDoUzI86Tm9V6e3b7kkzUi2I-LHZ6TygtCT_6zSTA8LUVJ/exec";
+const form = document.forms["submit-to-google-sheet"];
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    var formData = new FormData(form);
+    delayNotice()
+    fetch(scriptURL, { method: "POST", body: formData })
+        .then((response) => {
+            swal("Done", "Submitted Successfully.", "success");
+        })
+        .catch((error) => {
+            swal("Error", "Something went wrong. please try again!", "error");
+        });
+});
+
+// here is the delayNotice to artificially lag for 3seconds so google can connect
+
+function wait(ms = 0) {
+return new Promise(resolve => setTimeout(resolve, ms))
+}
+
+async function delayNotice() {
+sweetAlert({
+    title: "Submitting your query",
+    showCancelButton: false,
+    showConfirmButton: false
+})
+
+await wait(3000);
+console.log("done");
+sweetAlert.close();
+}
