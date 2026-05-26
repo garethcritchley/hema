d5864e3c849079acf2b1f70842b6b1912ff1503e
garethcritchley <theymightbegareths@gmail.com>
2026-05-27 09:49:35 +1000

    added contact to bottom of all pages and i updated contactus.js to specifically show progress pop up for transaction time not arbitrary time

--- assets/js/contactus.js
+++ assets/js/contactus.js
@@ -1,33 +1,27 @@
-const scriptURL =
-    "https://script.google.com/macros/s/AKfycbzPhOr2H_LAAqVdSaIHe00xo_hSxnoLMyJSf6qNzCz--eAQ5XFQBbhrF33UKIPm3mAe/exec";
+// replace this with the google app script address for the proper sheet,,,,
+const scriptURL =                       
+"https://script.google.com/macros/s/AKfycbzPhOr2H_LAAqVdSaIHe00xo_hSxnoLMyJSf6qNzCz--eAQ5XFQBbhrF33UKIPm3mAe/exec";
 const form = document.forms["submit-to-google-sheet"];
 form.addEventListener("submit", (e) => {
     e.preventDefault();
     var formData = new FormData(form);
-    delayNotice()
+
+    sweetAlert({
+        title: "Submitting your query",
+        showCancelButton: false,
+        showConfirmButton: false
+    })
+
     fetch(scriptURL, { method: "POST", body: formData })
-        .then((response) => {
-            swal("Done", "Submitted Successfully.", "success");
-        })
-        .catch((error) => {
-            swal("Error", "Something went wrong. please try again!", "error");
-        });
+    .then((response) => {
+        console.log("done");
+        sweetAlert.close();
+        swal("Done", "Submitted Successfully.", "success");
+    })
+    .catch((error) => {
+        console.log("done");
+        sweetAlert.close();
+        swal("Error", "Something went wrong. please try again!", "error");
+    });
 });
 
-// here is the delayNotice to artificially lag for 3seconds so google can connect
-
-function wait(ms = 0) {
-return new Promise(resolve => setTimeout(resolve, ms))
-}
-
-async function delayNotice() {
-sweetAlert({
-    title: "Submitting your query",
-    showCancelButton: false,
-    showConfirmButton: false
-})
-
-await wait(3000);
-console.log("done");
-sweetAlert.close();
-}
