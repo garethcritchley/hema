cdd22a97370efcf134aeb23010119e17e96c6926
garethcritchley <theymightbegareths@gmail.com>
2026-07-17 13:03:20 +1000

    new email

--- assets/js/contactus.js
+++ assets/js/contactus.js
@@ -1,6 +1,6 @@
 // replace this with the google app script address for the proper sheet,,,,
 const scriptURL =                       
-"https://script.google.com/macros/s/AKfycbzPhOr2H_LAAqVdSaIHe00xo_hSxnoLMyJSf6qNzCz--eAQ5XFQBbhrF33UKIPm3mAe/exec";
+"https://script.google.com/macros/s/AKfycbxX8dvYTyyuDB-fN3pzuYJ9aLwRXnp0PIWuGV48KaHOuwUM7rKYJAqgN9kPDscOeLPS/exec";
 const form = document.forms["submit-to-google-sheet"];
 form.addEventListener("submit", (e) => {
     e.preventDefault();
